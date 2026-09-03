3f970f44ee3bbdbd2d88b4c512a08a0252e3218d
wendymcameroniaggbs <120043927+wendymcameroniaggbs@users.noreply.github.com>
2026-09-03 13:08:17 +0100

    Moving server ts file

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,7 +1,7 @@
-import "./lib/error-capture";
+import "../lib/error-capture.ts";
 
-import { consumeLastCapturedError } from "./lib/error-capture";
-import { renderErrorPage } from "./lib/error-page";
+import { consumeLastCapturedError } from "../lib/error-capture.ts";
+import { renderErrorPage } from "../lib/error-page.ts";
 
 type ServerEntry = {
   fetch: (request: Request, env: unknown, ctx: unknown) => Promise<Response> | Response;
